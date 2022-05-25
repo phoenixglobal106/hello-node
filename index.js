d4d3ee5f7e140cb35c554db4e5809206139e13d6
phoenixglobal106 <phoenix.global106@gmail.com>
2022-05-25 18:56:27 +0530

    node-app

---
+++ index.js
@@ -0,0 +1,14 @@
+//import node framework
+
+var express = require('express');
+const res = require('express/lib/response');
+
+var app = express();
+
+app.get('/',function(req,res){
+    res.send('Hello world !!');
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
